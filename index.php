053d88d7f49d1f79bc1f5a4eae0a1329f0897182
Carlos Marques <99378021+CarlosMarques10@users.noreply.github.com>
2023-08-21 13:28:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+class conexao {
+
+private static $_pdoOracle;
+
+public static function getInstanceOracle() {
+
+    if (!isset(self::$_pdoOracle)) {
+        try {
+            $tnsName = '(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = ...)(PORT = 1521))) (CONNECT_DATA = (SID = ...)))';
+            $username = 'EBAHIANA';
+            $password = 'EB4H1AN4';
+            
+            self::$_pdoOracle = new \PDO('oci:dbname=' . $tnsName, $username, $password);
+            
+            // Verificar se a conexão foi estabelecida
+            if (self::$_pdoOracle) {
+                echo "Conexão estabelecida com sucesso!";
+            } else {
+                echo "Não foi possível estabelecer a conexão.";
+            }
+        } catch (\PDOException $e) {
+            echo "Erro de conexão: " . $e->getMessage();
+        }
+    }
+    return self::$_pdoOracle;
+}
+}
+
+ conexao::getInstanceOracle();
